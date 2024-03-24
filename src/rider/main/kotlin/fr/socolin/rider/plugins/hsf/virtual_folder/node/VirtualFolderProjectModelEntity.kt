d5a68a85ad5b8c8997435db5fc4c2379c5a3aabd
Socolin <provost.bertrand@gmail.com>
2024-03-24 16:42:55 -0400

    Use more logical value in VirtualFolderProjectModelEntity

--- src/rider/main/kotlin/fr/socolin/rider/plugins/hsf/virtual_folder/node/VirtualFolderProjectModelEntity.kt
+++ src/rider/main/kotlin/fr/socolin/rider/plugins/hsf/virtual_folder/node/VirtualFolderProjectModelEntity.kt
@@ -5,32 +5,37 @@ import com.intellij.platform.workspace.storage.EntitySource
 import com.intellij.platform.workspace.storage.WorkspaceEntity
 import com.intellij.platform.workspace.storage.url.VirtualFileUrl
 import com.jetbrains.rider.model.RdProjectModelItemDescriptor
+import com.jetbrains.rider.projectView.views.solutionExplorer.nodes.SolutionExplorerModelNode
 import com.jetbrains.rider.projectView.workspace.ProjectModelEntity
 import fr.socolin.rider.plugins.hsf.models.HsfHighlightingRule
 
 class VirtualFolderProjectModelEntity(
-    val sourceModelEntity: ProjectModelEntity,
-    val rule: HsfHighlightingRule
+    private val parentModelEntity: ProjectModelEntity,
+    val rule: HsfHighlightingRule,
 ) : ProjectModelEntity {
     override val alternativeUrls: List<VirtualFileUrl>
-        get() = sourceModelEntity.alternativeUrls
+        get() = parentModelEntity.alternativeUrls
     override val childrenEntities: List<ProjectModelEntity>
-        get() = sourceModelEntity.childrenEntities
+        get() = parentModelEntity.childrenEntities
     override val descriptor: RdProjectModelItemDescriptor
-        get() = VirtualFolderItemDescriptor(rule.virtualFolderName ?: "", sourceModelEntity.descriptor.location)
+        get() = VirtualFolderItemDescriptor(rule.virtualFolderName ?: "", parentModelEntity.descriptor.location)
     override val entitySource: EntitySource
-        get() = sourceModelEntity.entitySource
+        get() = VirtualEntitySource()
     override val parentEntity: ProjectModelEntity?
-        get() = sourceModelEntity.parentEntity
+        get() = parentModelEntity
     override val url: VirtualFileUrl?
-        get() = sourceModelEntity.url
+        get() = null
 
     override fun <E : WorkspaceEntity> createReference(): EntityReference<E> {
-        return sourceModelEntity.createReference()
+        return parentModelEntity.createReference()
     }
 
     override fun getEntityInterface(): Class<out WorkspaceEntity> {
-        return sourceModelEntity.getEntityInterface()
+        return parentModelEntity.getEntityInterface()
     }
+
+}
+
+class VirtualEntitySource : EntitySource {
 
 }
